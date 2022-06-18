d2854042b0ea8a6d0faaac54748e16da07b6f3e0
Pawel Paczkowski <paw.pac@yahoo.com>
2022-06-18 14:52:27 -0500

    Build function with string and number parmeters that will return a string specified amount of times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
-};
+const repeatString = function repeatString(string, num) {
+  let newString = "";
+    if (num < 0) {
+      newString = "ERROR";
+    } else {
+      for (i = 0; i < num; i++) {
+        newString = newString + string;
+      }
+    }
+  return newString;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
