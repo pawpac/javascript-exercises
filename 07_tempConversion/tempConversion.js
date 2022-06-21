69344d98888d945d331bdb3064af232878b87097
Pawel Paczkowski <paw.pac@yahoo.com>
2022-06-21 15:55:38 -0500

    Create 2 functions that will convert Fahrenheit to Celsius and vice versa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let result = (temp - 32) * (5/9);
+  if (result.toFixed(1) - result.toFixed(0) === 0) {
+  return Number(result.toFixed(0));
+  } else {
+    return Number(result.toFixed(1));
+  }
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let result = temp * (9/5) + 32;
+  if (result.toFixed(1) - result.toFixed(0) === 0) {
+  return Number(result.toFixed(0));
+  } else {
+    return Number(result.toFixed(1));
+  }
 };
 
 // Do not edit below this line
