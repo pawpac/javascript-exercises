97180c1fd1060727ede796932a544f51b616f172
Pawel Paczkowski <paw.pac@yahoo.com>
2022-06-21 07:16:39 -0500

    Solution with 'can remove all values' test failing

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(mainArray, ...toBeRemoved) {
+    let array = mainArray;
+    for (let i = 0; i < mainArray.length; i++) {
+        if (array.includes(toBeRemoved[i])) {
+            array.splice(array.indexOf(toBeRemoved[i]), 1)
+        }    
+    }
+    return array;
 };
 
 // Do not edit below this line
