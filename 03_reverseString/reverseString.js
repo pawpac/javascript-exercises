3bfdc20f61959b1a2ab355ce326663ac7397cf24
Pawel Paczkowski <paw.pac@yahoo.com>
2022-06-21 07:17:25 -0500

    Solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -4,3 +4,25 @@ const reverseString = function() {
 
 // Do not edit below this line
 module.exports = reverseString;
+
+
+
+
+
+// function splitStr(str) {
+//   let splited = str.split('');
+//   return splited;
+// }
+
+// splitStr("test");
+
+// function reverseArr() {
+//   const newArr = [];
+//   const oldArr = ['a', 'b', 'c'];
+//   for (let i = 0; i < oldArr.length; i++) {
+//       newArr.unshift(oldArr[i]);
+//       }
+//   return newArr;
+// }
+
+// reverseArr();
