777373b920250606ff2176eaa5e979887dc3f6fe
Pawel Paczkowski <paw.pac@yahoo.com>
2022-07-07 15:13:14 -0500

    Build add, subtract, sum, multiply, and factorial functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let result = 0;
+    for (let number of arr) {
+      result += number;
+    }
+    return result;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let result = 1;
+    for (let number of arr) {
+      result *= number;
+    }
+    return result;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	result = 1;
+    for (i = 1; i <= number; i++) {
+      result *= i;
+    }
+  return result;
 };
 
 // Do not edit below this line
