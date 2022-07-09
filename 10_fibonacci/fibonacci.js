fc67b07260764d600460a99be9e9bfc3206d69a0
Pawel Paczkowski <paw.pac@yahoo.com>
2022-07-08 21:27:13 -0500

    Create for loop that will push to an array user specified number of fibonacci members and use slice to return last member of that array

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(member) {
+  if (parseInt(member) < 0) {
+    return 'OOPS'
+  } else {
+    let first = 1;
+    let second = 1;
+    let arr = [];
 
+    arr.push(first);
+    arr.push(second);
+
+    for (let i = 0; i < (parseInt(member)-2); i++) {
+      let outcome = first + second;
+      arr.push(outcome);
+
+      first = second;
+      second = outcome;
+    }
+  return parseInt(arr.slice(-1));
+}
 };
 
 // Do not edit below this line
