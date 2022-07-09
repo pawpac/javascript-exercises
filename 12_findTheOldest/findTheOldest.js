bce22d6af304c93bbd7fcd990654c0d0d62debab
Pawel Paczkowski <paw.pac@yahoo.com>
2022-07-09 11:53:25 -0500

    Iterate through array to check if yearOfDeath is undefined. If it is undefined add 'today' key and set it to 2022. Check for age and return oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+  let person = '';
+  let oldest = 0;
+  let age = 0;
 
+  for (object of array) {
+    if (object.yearOfDeath === undefined) {
+      object.today = 2022;
+      age = object.today - object.yearOfBirth;
+    } else {
+      age = object.yearOfDeath - object.yearOfBirth;
+    }
+      if (age > oldest) {
+        person = object.name;
+        oldest = age;
+      }
+  }
+  return person;
 };
 
 // Do not edit below this line
