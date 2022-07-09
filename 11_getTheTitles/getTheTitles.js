86582dc4615d2feece49f09f4dc4f1d7348c20fa
Pawel Paczkowski <paw.pac@yahoo.com>
2022-07-08 22:00:02 -0500

    Use for loop to iterate through an array of objects and push titles from those objects to new array

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+  newArr = [];
+  for (object of arr) {
+    newArr.push(object.title);
+  }
+  return newArr;
 };
 
 // Do not edit below this line
